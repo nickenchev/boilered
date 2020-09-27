1fcc2c42a11057af55517c65a3aee269e8da53fa
Nick Enchev <nikolaenchev@gmail.com>
2020-09-26 20:30:37 -0400

    Starting material list model

---
+++ src/materiallistmodel.h
@@ -0,0 +1,14 @@
+#ifndef MATERIALLISTMODEL_H
+#define MATERIALLISTMODEL_H
+
+#include <QAbstractListModel>
+
+class MaterialListModel : public QAbstractListModel
+{
+public:
+    MaterialListModel();
+    virtual ~MaterialListModel();
+};
+
+
+#endif /* MATERIALLISTMODEL_H */
